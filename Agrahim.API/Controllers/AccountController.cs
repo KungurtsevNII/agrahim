94fa1b0a0a39a8a9b5f27afa72a16af4c3c7b8fb
Никита Кунгурцев <KungurtsevNI@gmail.com>
2020-01-08 15:31:41 +0400

    Add account controller. Add feature change password.

---
+++ Agrahim.API/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Agrahim.Common.ViewModels;
+using Agrahim.Infrastructure.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agrahim.API.Controllers
+{
+    [Route("account")]
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<UserEntity> _userManager;
+
+        public AccountController(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("change-password")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+            if (user is null)
+            {
+                return BadRequest("Пользователь не найден");
+            }
+
+            var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+
+            // Если изменение закончилось с ошибками, то сохраняем их в модель.
+            if (!changePasswordResult.Succeeded)
+            {
+                foreach (var error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                
+            }
+            
+            return View("Index");
+        }
+    }
+}
---
+++ Agrahim.Common/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agrahim.Common.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Текущий пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Новый пароль")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
